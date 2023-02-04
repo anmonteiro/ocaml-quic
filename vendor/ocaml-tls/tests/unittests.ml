62facc7205e9cea12df05e517d8a8493626a8648
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-02-04 12:09:26 -0800

    rm ocaml-tls

--- vendor/ocaml-tls/tests/unittests.ml
+++
@@ -1,8 +0,0 @@
-open OUnit2
-
-let suite =
-  "All" >::: [
-    "Reader" >::: Readertests.reader_tests ;
-    "Writer" >::: Writertests.writer_tests ;
-    "ReaderWriter" >::: Readerwritertests.readerwriter_tests ;
-  ]
