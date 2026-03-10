e0b21dc44e6c5beb93682bd5034884ecc852e387
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-03-09 22:34:45 -0700

    dep on ssl

--- flake.nix
+++ flake.nix
@@ -13,7 +13,18 @@
           let
             pkgs = nixpkgs.legacyPackages.${system}.extend (
               self: super: {
-                ocamlPackages = super.ocaml-ng.ocamlPackages_5_4;
+                ocamlPackages = super.ocaml-ng.ocamlPackages_5_4.overrideScope (
+                  oself: osuper: {
+                    ssl = osuper.ssl.overrideAttrs (_: {
+                      src = super.fetchFromGitHub {
+                        owner = "savonet";
+                        repo = "ocaml-ssl";
+                        rev = "a3ec4b6d6883a6a73e59f6756eceb1b7cbf45183";
+                        hash = "sha256-zXk5cV6lz5q6XX/CVk8ymt/o+J8DCgAqWMULJLPzenk=";
+                      };
+                    });
+                  }
+                );
               }
             );
           in
